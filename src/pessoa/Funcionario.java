c6815236abbad7ebf69d57b3124835e77af8763c
BiaF <51976516+BiaF@users.noreply.github.com>
2019-08-01 22:33:48 -0300

    Construtor com senha
    Coloquei a senha no construtor

--- src/pessoa/Funcionario.java
+++ src/pessoa/Funcionario.java
@@ -2,8 +2,8 @@ package src.pessoa;
 
 public abstract class Funcionario extends Pessoa throws VazioException {
 	
-	public Funcionario(String nome, String id) throws VazioException {
-		super(nome, id);
+	public Funcionario(String nome, String senha, String id) throws VazioException {
+		super(nome, senha, id);
 	}
 
 }
