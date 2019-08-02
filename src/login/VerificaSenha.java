8844494a52ea95b6660195f0ac75f109b111180d
mazucanti <daniel.mazucanti.ismart@gmail.com>
2019-08-02 14:52:45 -0300

    Classe alterada para instanciar classe de login

--- src/login/VerificaSenha.java
+++ src/login/VerificaSenha.java
@@ -1,4 +1,5 @@
 package login;
+import pessoa.*;
 import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -14,8 +15,8 @@ public class VerificaSenha implements IMetodoAutenticacao {
         while(scan.hasNext()){
             String line = scan.nextLine().toString();
             if(line.contains(id)){
-		    String[] pswd = line.split(", ");
-            	return pswd[1];
+		    String[] pswd = line.split(",");
+            	return pswd[1].trim();
 	    }
         }
 		return null;
@@ -30,35 +31,73 @@ public class VerificaSenha implements IMetodoAutenticacao {
 			    
 		System.out.println("Senha:");
 		String senha = scanner.next(); 
+
+		Scanner instancia;
 		
 		String retornoSenha = null;
 		String StringAcesso = null;
 		switch(id.charAt(0)) {
-		case 1:
-			retornoSenha = procuraAcesso("../../db/listaPaciente.txt", id);
-			StringAcesso = "Paciente";
-			break;
-		case 2:
-			retornoSenha = procuraAcesso("../../db/listaMedico.txt", id);
-			StringAcesso = "Medico";
-			break;
-		case 3:
-			retornoSenha = procuraAcesso("../../db/listaTexEnfermagem.txt", id);
-			StringAcesso = "Tecnico de Enfermagem";
-			break;
-		case 4:
-			retornoSenha = procuraAcesso("../../db/listaAtendente.txt", id);
-			StringAcesso = "Atendente";
-			break;
-		case 5:
-			retornoSenha = procuraAcesso("../../db/listaGerente.txt", id);
-			StringAcesso = "Gerente";
-			break;
+
+			case 1:
+				retornoSenha = procuraAcesso("../../db/listaPaciente.txt", id);
+				instancia = new Scanner(new File("../../db/listaPaciente.txt"));
+				while(instancia.hasNext()){
+					String registro = instancia.nextLine().toString();
+					if(registro.cointais(id)){
+						String[] args = registro.split(", ");
+					}
+				}
+				Paciente user = new Paciente(args[0],args[1],args[2],args[3],args[4],Integer.parseInt(args[5]),Float.parseFloat(args[6]),Integer.parseInt(args[7]),args[8],args[9]);
+				break;
+			case 2:
+				retornoSenha = procuraAcesso("../../db/listaMedico.txt", id);
+				instancia = new Scanner(new File("../../db/listaMedico.txt"));
+				while(instancia.hasNext()){
+					String registro = instancia.nextLine().toString();
+					if(registro.cointais(id)){
+						String[] args = registro.split(", ");
+					}
+				}
+				Medico user = new Medico(args[0],args[1],args[2],args[3]);
+				break;
+			case 3:
+				retornoSenha = procuraAcesso("../../db/listaTecEnfermagem.txt", id);
+				instancia = new Scanner(new File("../../db/listaTecEnfermagem.txt"));
+				while(instancia.hasNext()){
+					String registro = instancia.nextLine().toString();
+					if(registro.cointais(id)){
+						String[] args = registro.split(", ");
+					}
+				}
+				String[] argvar = Arrays.copyOfRange(args,3,args.length);
+				TecEnfermagem user = new TecEnfermagem(args[0].args[1],args[2],argvar);
+				break;
+			case 4:
+				retornoSenha = procuraAcesso("../../db/listaAtendente.txt", id);
+				instancia = new Scanner(new File("../../db/listaAtendente.txt"));
+				while(instancia.hasNext()){
+					String registro = instancia.nextLine().toString();
+					if(registro.cointais(id)){
+						String[] args = registro.split(", ");
+					}
+				}
+				Atendente user = new Atendente();
+				break;
+			case 5:
+				retornoSenha = procuraAcesso("../../db/listaGerente.txt", id);
+				instancia = new Scanner(new File("../../db/listaGerente.txt"));
+				while(instancia.hasNext()){
+					String registro = instancia.nextLine().toString();
+					if(registro.cointais(id)){
+						String[] args = registro.split(", ");
+					}
+				}
+				Gerente user = new Gerente(args[0],args[1],args[2]);
+				break;
 		}
-		
 		scanner.close();
 		if(senha.equals(retornoSenha)){
-			return new UsuarioAutenticado(id, StringAcesso);
+			return user;
 		}
 		else {
 			throw new UsuarioNaoAutenticadoException();
