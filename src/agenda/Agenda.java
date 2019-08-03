3a3148803ad476b872670dd13b36c3f76cd0bb95
Raygagz <33377462+Raygagz@users.noreply.github.com>
2019-08-03 20:45:25 -0300

    O arquivo agora é apagado
    Tirei *.class do .gitignore para vocês verem a compilação
    
    Para ver o arquivo apagando é só criar um "agenda.txt" vazio em db e dar "java testeAgenda" dentro de bin

--- src/agenda/Agenda.java
+++ src/agenda/Agenda.java
@@ -17,7 +17,7 @@ public class Agenda {
 	private static Agenda agenda;
 	
 	private Agenda() throws IOException {
-		br = new BufferedWriter(new FileWriter("agenda.txt"));
+		br = new BufferedWriter(new FileWriter("../db/agenda.txt"));
 		br.write("AGENDA 2019");
 		br.newLine();
 		br.flush();
@@ -32,7 +32,7 @@ public class Agenda {
 	private String recuperaNome(String idPac) throws FileNotFoundException{
 		String nomePaciente = "";
 		
-		Scanner scanPac = new Scanner(new File("listaPaciente.txt"));
+		Scanner scanPac = new Scanner(new File("../db/listaPaciente.txt"));
 		while(scanPac.hasNext()) {
 			String linePac = scanPac.nextLine().toString();
 			if(linePac.contains(idPac)) {
@@ -48,7 +48,7 @@ public class Agenda {
 		
 			//Recupera nome do médico por meio do id
         if(args[2].equals("consulta") || args[2].equals("Consulta")) {
-        	Scanner scanMed = new Scanner(new File("listaMedico.txt"));
+        	Scanner scanMed = new Scanner(new File("../db/listaMedico.txt"));
         	while(scanMed.hasNext()) {
         		String lineMed = scanMed.nextLine().toString();
         		if(lineMed.contains(args[3])) {
@@ -60,7 +60,7 @@ public class Agenda {
         }
         else { 
         	//Recupera nome do tecnico por meio do id
-        	Scanner scanTec = new Scanner(new File("listaTecEnfermagem.txt"));
+        	Scanner scanTec = new Scanner(new File("../db/listaTecEnfermagem.txt"));
         	while(scanTec.hasNext()) {
         		String lineTec = scanTec.nextLine().toString();
         		if(lineTec.contains(args[3])) {
@@ -74,22 +74,22 @@ public class Agenda {
 	}
 	
 	public void visualizaAgenda() throws FileNotFoundException {	
-		Scanner scan = new Scanner(new File("agenda.txt"));
+		Scanner scan = new Scanner(new File("../db/agenda.txt"));
 		String line = scan.nextLine().toString();
 		System.out.println(line);
 		while(scan.hasNext()){
-            line = scan.nextLine().toString();
-            String[] args = line.split(", ");
-            String nomeProfissional = recuperaNome(args);
+			line = scan.nextLine().toString();
+			String[] args = line.split(", ");
+			String nomeProfissional = recuperaNome(args);
 			String nomePaciente = recuperaNome(args[4]);
-            System.out.println("[" + args[0] + "] [" + args[1] +"] >> " + args[2] + ", Profissional: " + nomeProfissional + ", Paciente: " + nomePaciente);
-        }
+			System.out.println("[" + args[0] + "] [" + args[1] +"] >> " + args[2] + ", Profissional: " + nomeProfissional + ", Paciente: " + nomePaciente);
+		}
 		scan.close();
 	}
 	
 	public void visualizaAgenda(String id) throws FileNotFoundException {
 			
-		Scanner scan = new Scanner(new File("agenda.txt"));
+		Scanner scan = new Scanner(new File("../db/agenda.txt"));
 		String line = scan.nextLine().toString();
 		System.out.println(line);
 		while(scan.hasNext()){
@@ -109,7 +109,7 @@ public class Agenda {
 		String linha;
 		int quant = 0; 
 
-		buff = new BufferedReader(new FileReader("agenda.txt"));
+		buff = new BufferedReader(new FileReader("../db/agenda.txt"));
 		try {
 			linha = buff.readLine();
 			while (linha != null && quant < agenda.length-1) {
@@ -126,7 +126,7 @@ public class Agenda {
 	
 	public void desmarcaAgenda(String id, String data, String horario) throws IOException{
 		
-		Scanner scan = new Scanner(new File("agenda.txt"));
+		Scanner scan = new Scanner(new File("../db/agenda.txt"));
 		String linha = null;
 		while(scan.hasNext()){
 			String line = scan.nextLine().toString();
@@ -148,7 +148,7 @@ public class Agenda {
 			i++;
 		}
 		
-		File file = new File("agenda.txt");
+		File file = new File("../db/agenda.txt");
 		file.delete();
 		/*Path src = Paths.get(file.getAbsolutePath());
 		String str_dest = file.getAbsolutePath()+".bad";
@@ -171,7 +171,7 @@ public class Agenda {
 	public void marcaAgenda(String data, String horario, String tipo, String idFunc, String idPac) throws FileNotFoundException{
 		try {			
 			String novaLinha = data + ", "+horario+", " + tipo + ", " + idFunc + ", " + idPac;
-			Scanner scan = new Scanner(new File("agenda.txt"));
+			Scanner scan = new Scanner(new File("../db/agenda.txt"));
 			while(scan.hasNext()){
 				String line = scan.nextLine().toString();
 				if(line.contains(novaLinha)){
